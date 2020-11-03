a934ef1bd4f4a78d52c2b3ba88d970867493b6a7
wathigo <wathigosimon@gmail.com>
2020-11-03 10:23:02 -0500

    set up redux store

--- src/index.js
+++ src/index.js
@@ -7,9 +7,11 @@ import { Provider } from 'react-redux'
 import store from './redux/store'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
 
